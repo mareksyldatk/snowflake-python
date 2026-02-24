fdece2aa06b17f58205b26a58fc1eb0c6790c249
Marek Syldatk <marek@syldatk.com>
2026-02-24 22:20:05 +0100

    Add sql+python demo

--- run_demo_python.sql
+++ run_demo_python.sql
@@ -1,40 +1,17 @@
--- Demo: run Python from this Git-backed repository in Snowflake
---
--- Prerequisites:
+-- Minimal demo: run Python from this Git-backed repository in Snowflake.
+-- Assumes these were already run:
 -- 1) sql/bootstrap_prod.sql
 -- 2) sql/bootstrap_git_integration.sql
---
--- This script can be rerun.
 
--- ------------------------------------------------------------------
--- 1) Ensure ROLE_PROD_PYTHON can use integration + secret
--- ------------------------------------------------------------------
-USE ROLE ACCOUNTADMIN;
-GRANT USAGE ON INTEGRATION GITHUB_INT_SNOWFLAKE_PYTHON TO ROLE ROLE_PROD_PYTHON;
-
-USE ROLE SECURITYADMIN;
-GRANT READ ON SECRET ANALYTICS_PROD.SECURITY.GITHUB_PAT_SECRET TO ROLE ROLE_PROD_PYTHON;
-
--- ------------------------------------------------------------------
--- 2) Create/fetch Git repository object
--- ------------------------------------------------------------------
 USE ROLE ROLE_PROD_PYTHON;
 USE WAREHOUSE WH_PROD_PYTHON;
 USE DATABASE ANALYTICS_PROD;
 
-CREATE OR REPLACE GIT REPOSITORY ANALYTICS_PROD.INTEGRATION.REPO_SNOWFLAKE_PYTHON
-  API_INTEGRATION = GITHUB_INT_SNOWFLAKE_PYTHON
-  GIT_CREDENTIALS = ANALYTICS_PROD.SECURITY.GITHUB_PAT_SECRET
-  ORIGIN = 'https://github.com/mareksyldatk/snowflake-python.git';
-
 ALTER GIT REPOSITORY ANALYTICS_PROD.INTEGRATION.REPO_SNOWFLAKE_PYTHON FETCH;
 
--- Optional: inspect fetched files.
+-- Optional check:
 LS @ANALYTICS_PROD.INTEGRATION.REPO_SNOWFLAKE_PYTHON/branches/main;
 
--- ------------------------------------------------------------------
--- 3) Create and run Python procedure from repo file
--- ------------------------------------------------------------------
 USE SCHEMA ANALYTICS_PROD.PYTHON;
 
 CREATE OR REPLACE PROCEDURE DEMO_REPO_PY()
---
+++ sql/bootstrap_git_repository.sql
@@ -0,0 +1,13 @@
+-- First run bootstrap: create Snowflake Git repository object
+-- Assumes:
+-- 1) sql/bootstrap_prod.sql
+-- 2) sql/bootstrap_git_integration.sql
+
+USE ROLE ROLE_PROD_PYTHON;
+USE DATABASE ANALYTICS_PROD;
+USE SCHEMA INTEGRATION;
+
+CREATE GIT REPOSITORY IF NOT EXISTS REPO_SNOWFLAKE_PYTHON
+  API_INTEGRATION = GITHUB_INT_SNOWFLAKE_PYTHON
+  GIT_CREDENTIALS = ANALYTICS_PROD.SECURITY.GITHUB_PAT_SECRET
+  ORIGIN = 'https://github.com/mareksyldatk/snowflake-python.git';
